451178fd4452d051e874d659f548674912dcb5e4
Cloud User <centos@oc-centos-14.compute-a418969.oraclecloud.internal>
2016-10-22 21:48:08 -0200

    v 1.0 ... It is runnig like a cham

--- battleship.py
+++ battleship.py
@@ -2,39 +2,37 @@
 
 import random
 import sys
+import os
 
 def check_boat_board(_board_boats, _boat_size, _boat_x, _boat_y, _boat_hv, _board_x, _board_y):
-	#print_board(_board_boats, _board_x, _board_y)
-
-	#print "_boat_size : "+ str(_boat_size)
-	#print "_boat_x : " + str(_boat_x)
-	#print "_boat_y : " + str(_boat_y)
-	#print "_boat_hv : " + str(_boat_hv)
-	#print "_board_x : " + str(_board_x)
-	#print "_board_y : " + str(_board_y)
-
+	# Check Horizontal cases
 	if ( _boat_hv == "h" ):
+		# If the boat does not fit
 		if ( _boat_x + _boat_size > _board_x - 1 ):
 			return False
 
+		# If the choosen positions are not available
 		for z in range(_boat_size):
-			#print "z: " + str(z)
 			if ( _board_boats[_boat_x + z][_boat_y] != "." ):
 				return False
+	# Check Vertical cases
 	if ( _boat_hv == "v" ):
+		# If the boat does not fit
 		if ( _boat_y + _boat_size > _board_y - 1):
 			return False
 
+		# If the choosen positions are not available
 		for z in range(_boat_size):
-			#print "z: " + str(z)
 			if ( _board_boats[_boat_x][_boat_y + z] != "." ):
 				return False
 
+	# The boat is ok to the position
 	return True
 
 def print_board(_board_to_print, _x, _y):
 	for b in range(_y):
 		for a in range(_x):
+			# Print each position
 			print _board_to_print[a][b],
 
 		print " "
@@ -44,11 +42,9 @@ def main():
 	#board_xy_options = [1, 2, 3, 4, 6]
 	board_xy_options = [10, 12, 14, 16, 18]
 
-	# Ships by their:
-	# Sizes
+	# Ships sizes and hit counters
 	boats_size = {"A":6, "B":4, "C":4, "D":3, "E":3, "F":3, "G":2, "H":2, "I":2}
-	# Status 
-	boats_active = {"A":False, "B":False, "C":False, "D":False, "E":False, "F":False, "G":False, "H":False, "I":False}
+	boats_hit_counter = boats_size
 
 	# Orientation options
 	boats_hv_options = ["h", "v"]
@@ -57,15 +53,12 @@ def main():
 	board_x = random.choice(board_xy_options)
 	board_y = random.choice(board_xy_options)
 
-	print "Board x , y : %d , %d" % (board_x, board_y)
-	print " "
-
-	# Filling the board for bets with dots
-	board_bets = [[] for _ in range(board_x)]
+	# Filling the shots board, with dots
+	board_shots = [[] for _ in range(board_x)]
 
 	for a in range(board_x):
 		for b in range(board_y):
-			board_bets[a].append(".")
+			board_shots[a].append(".")
 
 	# Filling the board for boats placement with dots
         board_boats = [[] for _ in range(board_x)]
@@ -84,12 +77,9 @@ def main():
 			boat_hv = random.choice(boats_hv_options)
 
 			# Call the function to validate the boat on the board
-			#print "Validating boat: " + boat 
 			boat_ok = check_boat_board(board_boats, boats_size[boat], boat_x, boat_y, boat_hv, board_x, board_y)
 
 		# Placing, after validation
-		#print "Placing the boat: " + boat
-
 		if ( boat_hv == "h" ):
 			for z in range(boats_size[boat]):
 				board_boats[boat_x + z][boat_y] = boat
@@ -98,34 +88,101 @@ def main():
 			for z in range(boats_size[boat]):
 				board_boats[boat_x][boat_y + z] = boat
 
-	# Printing the boats board
-	print " "
-        print "Boats board:"
-	print_board(board_boats, board_x, board_y)
+	# Loop for the shots
+	while ( True ):
 
-	# Printing the bets board
-	print " "
-	print "Bets board:"
-	print_board(board_bets, board_x, board_y)
+		# Printing the current scenario
+		os.system('clear')
+		print " "
+		print "X size: " +  str(board_x)
+		print "Y size: " +  str(board_y)
 
+		# Please, uncomment the lines bellow, if you want to show the boats position
+		#print " "
+		#print "Boats board:"
+		#print_board(board_boats, board_x, board_y)
 
-	# Validating the boats
-	#is_boats_available = False
+		# Printing the shots board
+		print " "
+		print "Board:"
+		print_board(board_shots, board_x, board_y)
 
-	#print " "
-	#print "Boats:"
+		# Check if all boats are sunk
+		all_sunk = True
 
-	#for boat in boats_size.keys():
-	#	if ( boats_size[boat] <= board_x or boats_size[boat] <= board_y ):
-	#		is_boats_available = True
-	#		boats_active[boat] = True
+		for boat in boats_hit_counter.keys():
+			if ( boats_hit_counter[boat] > 0 ):
+				all_sunk = False			
 
-	#	print str(boat) + ": size " + str(boats_size[boat]) + " is " + str(boats_active[boat])
+		# If so, the game is over
+		if ( all_sunk ):
+			print " "
+			print "You win !!!!"
+			sys.exit(0)
 
-	#if not is_boats_available:
-	#	print "The board size doesn't allow to place any boat, sorry."
-	#	sys.exit(0)
+		# Getting the x coordinate
+		print " "
+		print "Please, provide the coordinates for your shot (Use 'q' to quit):"
 
+		print "x: ",
+		input_x = raw_input()
+
+		if ( input_x == "q" ):
+			sys.exit(0)
+		
+		# Getting the y coordinate
+                print "y: ",
+                input_y = raw_input()
+
+                if ( input_y == "q" ):
+                        sys.exit(0)
+
+		# Converting the x coordinate on an integer value
+		try:
+			shot_x = int(input_x) - 1
+		except:
+			shot_x = 0
+
+		# Converting the y coordinate on an integer value
+		try:
+			shot_y = int(input_y) - 1
+		except:
+			shot_y = 0
+
+		# Validating if the shot is valid
+		if ( shot_x >= 0 and shot_x < board_x and shot_y >= 0 and shot_y < board_y ):
+			# Validating if the shot position is available
+			if ( board_shots[shot_x][shot_y] != "." ):
+				print " "
+				print "Sorry, you already did this shot."
+				print "Please, hit <enter>."
+                        	aa = raw_input()
+			else:
+				# Validating if the player hit a boat
+				if ( board_boats[shot_x][shot_y] != "." ):
+					board_shots[shot_x][shot_y] = "x"
+
+					boat = board_boats[shot_x][shot_y]
+
+					boats_hit_counter[boat] -= 1
+
+					# Validating if the boat is sunk
+					if ( boats_hit_counter[boat] <= 0 ):
+						boats_hit_counter[boat] = 0
+
+						# If so, print the boat on the board
+						for a in range(board_x):
+							for b in range(board_y):
+								if ( board_boats[a][b] == boat ):
+									board_shots[a][b] = boat
+				else:
+					# Mark the position as "water"
+					board_shots[shot_x][shot_y] = "~"
+		else:
+			print " "
+			print "Sorry, the coordinates x,y are out of the board."
+			print "Please, hit <enter>."
+			aa = raw_input()
 
 if __name__=="__main__":
 	main()
